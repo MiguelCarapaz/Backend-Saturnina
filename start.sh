6131c53c88edeeba3db0f2d38e1d66ac384755bb
MiguelCarapaz <85420759+MiguelCarapaz@users.noreply.github.com>
2025-07-31 16:45:43 -0500

    add archive start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# start.sh
+
+# Instalar dependencias
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Ejecutar la aplicación
+uvicorn app.main:app --host 0.0.0.0 --port 10000
