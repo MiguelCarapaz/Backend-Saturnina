f637d71a083b17b1fed848d712f32969c334666e
MiguelCarapaz <85420759+MiguelCarapaz@users.noreply.github.com>
2025-08-01 18:23:15 -0500

    add cors

--- main.py
+++ main.py
@@ -8,12 +8,19 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "https://saturnina.vercel.app",  
+    "http://localhost:3000",        
+    "http://127.0.0.1:3000"          
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://saturnina.vercel.app"],  # solo tu frontend en producción
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+    expose_headers=["*"] 
 )
 
 # Incluye tus routers aquí
