c3de76bed7ddaa5e60feea92fca09df9c259d9d1
MiguelCarapaz <85420759+MiguelCarapaz@users.noreply.github.com>
2025-08-03 18:55:46 -0500

    database

--- app/database.py
+++ app/database.py
@@ -11,8 +11,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(
     DATABASE_URL,
-    echo=True,
-    future=True,
     connect_args={"statement_cache_size": 0}
 )
 SessionLocal = sessionmaker(
