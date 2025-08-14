892daf1f52c3aa1eb0c454f4ced43be7e00d346b
MiguelCarapaz <85420759+MiguelCarapaz@users.noreply.github.com>
2025-08-13 21:55:21 -0500

    update orders

--- app/models/orders.py
+++ app/models/orders.py
@@ -1,9 +1,7 @@
 from sqlalchemy import Column, Integer, Numeric, String, ForeignKey, DateTime, Text
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
+from sqlalchemy.sql import func
+from app.database import Base
 
 class Order(Base):
     __tablename__ = "orders"
@@ -11,8 +9,16 @@ class Order(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
     total = Column(Numeric, nullable=False)
     status = Column(String, default="pendiente")
+    nombre = Column(String, nullable=True)
+    apellido = Column(String, nullable=True)
+    direccion = Column(String, nullable=True)
+    email = Column(String, nullable=True)
+    telefono = Column(String, nullable=True)
+    descripcion = Column(Text, nullable=True)
+    image_transaccion = Column(Text, nullable=True)
     created_at = Column(DateTime(timezone=False), server_default=func.now())
-    items = relationship("OrderItem", back_populates="order")
+
+    items = relationship("OrderItem", back_populates="order", cascade="all, delete-orphan")
 
 class OrderItem(Base):
     __tablename__ = "order_items"
@@ -21,4 +27,7 @@ class OrderItem(Base):
     product_id = Column(Integer, ForeignKey("products.id"))
     quantity = Column(Integer, nullable=False)
     price = Column(Numeric, nullable=False)
+    talla = Column(String, nullable=True)
+    color = Column(String, nullable=True)
+
     order = relationship("Order", back_populates="items")
