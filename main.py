bc464c6df4cb179ae06079af550c627d6810899f
MiguelCarapaz <85420759+MiguelCarapaz@users.noreply.github.com>
2025-08-20 20:09:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://saturnina.vercel.app"], "http://localhost:5173"],
+    allow_origins=["https://saturnina.vercel.app", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
