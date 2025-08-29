bb0ba22f33774203fdfe6ab92275675735b2d64a
MiguelCarapaz <85420759+MiguelCarapaz@users.noreply.github.com>
2025-08-28 19:05:23 -0500

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# start.sh
-
-# Instalar dependencias
-pip install --upgrade pip
-pip install -r requirements.txt
-
-# Ejecutar la aplicación
-uvicorn main:app --host 0.0.0.0 --port 10000
